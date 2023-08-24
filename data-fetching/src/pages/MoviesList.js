cf57465a19d6ae1be943c1c3612b6d5fae7986a6
Paulo Celis <pauloxyo@gmail.com>
2023-08-24 10:37:15 -0600

    Data Fetching

---
+++ data-fetching/src/pages/MoviesList.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import Movie from './Movie';
+import styles from '../styles/MoviesList.module.css';
+
+const MovieList = (props) => {
+  return (
+    <ul className={styles['movies-list']}>
+      {props.movies.map((movie) => (
+        <Movie
+          key={movie.id}
+          title={movie.title}
+          releaseDate={movie.releaseDate}
+          openingText={movie.openingText}
+        />
+      ))}
+    </ul>
+  );
+};
+
+export default MovieList;
